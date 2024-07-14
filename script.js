6e58f599a0c35660244636dffa9ad449bdaab18e
A-lpha18 <163670986+A-lpha18@users.noreply.github.com>
2024-07-14 12:17:37 +0530

    added index.html

--- script.js
+++ script.js
@@ -6,9 +6,25 @@ function billPay(){
     var f5=document.getElementById("name1").value;
 
     var name=[f1,f2,f3,f4,f5];
-    var numberofName=name.length;
     var n=Math.random();
     n=n*4;
     var numberofPerson=Math.floor(n);
-    console.log(n);
+    if(numberofPerson=0){
+        document.getElementById("print").innerHTML=f1+" will pay the bill";
+    }
+    else if(numberofPerson=1){
+        document.getElementById("print").innerHTML=f2+" will pay the bill";
+    }
+    else if (numberofPerson=2){
+        document.getElementById("print").innerHTML=f3+" will pay the bill";
+    }
+    else if(numberofPerson=3){
+        document.getElementById("print").innerHTML=f4+" will pay the bill";
+    }
+    else if(numberofPerson=4){
+        document.getElementById("print").innerHTML=f5+" will pay the bill";
+    }
+}
+function reload(){
+    location.reload();
 }
